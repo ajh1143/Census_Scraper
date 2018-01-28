28ed8e7014ef2a9915519ad152243e8fcbf7ed9f
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-01-28 13:41:50 -0800

    Update census_scraper.py

--- census_scraper.py
+++ census_scraper.py
@@ -13,14 +13,19 @@ def file_location():
     file_output_location = input("Enter a directory for output of generated text files.")
     return file_output_location
 
-#Scraping method, iterates through each web page based on decade and creates a dict of the scraped data to be passed to df_print()
+#Scraping method, iterates through each web page based on decade and creates a dict of the scraped 
+#data to be passed to df_print()
 def core_logic():
     output = file_location()
-
+    #For each decade in range 1880-2010
     for decade in range(1880, 2020, 10):
+        #Pass correct webpage based on decade
         page_decade = "https://www.ssa.gov/oact/babynames/decades/names"+str(decade)+"s.html"
+        #Open URL w/ BeautifulSoup
         soup = BeautifulSoup(urlopen(page_decade))
+        #Assign targets to parse
         targets = soup.find_all("tr",{"align":"right"})[1:]
+        #Create empty Dictionary to add results
         data = {
             'Rank' : [],
             'Name_Male' : [],
@@ -28,10 +33,13 @@ def core_logic():
             'Name_Female' : [],
             'Number_Female' : []
         }
-
+        #For each target dataset
         for target in targets:
+            #Extract text
             target_acquired = target.get_text()
+            #Split target values
             z = target_acquired.split()
+            #Append to each category
             data['Rank'].append(z[0])
             data['Name_Male'].append(z[1])
             data['Number_Male'].append(z[2])
@@ -41,14 +49,22 @@ def core_logic():
 
 #Transforms the dataset into a DataFrame using Pandas, prints to the previously acquired output location        
 def df_print(file, year, populated_data_dict):
+    #Assign output location/filename
     file_output = file
+    #Assign current year
     decade = year
+    #Assign the dictionary to data
     data = populated_data_dict
+    #Allow max rows
     pd.set_option('display.max_rows', 1000)
+    #Create DataFrame
     df = pd.DataFrame(data)
+    #Assign datapoints to columns
     df = df[['Rank', 'Name_Male','Number_Male','Name_Female','Number_Female']]
+    #Print a local csv copy to sepcified location, stored as .txt file named for each decade
     sys.stdout = open(file_output+"/"+str(decade)+".txt", "w")
     print (df)
+    #Close output
     sys.stdout.close()
 
 if __name__ == '__main__':
